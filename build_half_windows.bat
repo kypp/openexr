406e00fa4987b97a8249dc3e722c89c5ede10bca
kypp <kyp108@gmail.com>
2014-09-04 15:47:05 +0200

    added VS2013 build command (reqires cmake in path)
    added gitignore for vs files

---
+++ build_half_windows.bat
@@ -0,0 +1,13 @@
+mkdir lib
+set libdir=%cd%\lib
+cd IlmBase/Half
+mkdir vs
+cd vs
+cmake -G "Visual Studio 12" ..
+call "%programfiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+set include=%cd%;%include%
+msbuild /p:useenv=true;configuration=debug Half.vcxproj
+msbuild /p:useenv=true;configuration=release Half.vcxproj
+echo %libdir%
+xcopy Debug %libdir%\Debug\ /y
+xcopy Release %libdir%\Release\ /y
